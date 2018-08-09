e1985efe374ca89640b7830402b27f5f7e2c514e
Oleksii Popov <alexejpopovua@gmail.com>
2018-08-09 21:23:55 +0200

    Points with clusters using a library

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -25,6 +25,9 @@ export default class MapView {
         this.map = new mapboxgl.Map({
             container: this.el.firstChild,
             style: {
+                "glyphs": "http://glfonts.lukasmartinelli.ch/fonts/{fontstack}/{range}.pbf",
+                //"glyphs": "http://fonts.openmaptiles.org/{fontstack}/{range}.pbf",
+                //"glyphs": "mapbox://fonts/mapbox/{fontstack}/{range}.pbf",
                 version: 8,
                 zoom: 8, // default zoom.
                 center: [0, 51.5], // default center coordinate in [longitude, latitude] format.
@@ -55,44 +58,94 @@ export default class MapView {
     }
 
     addEventListeners() {
-        this.map.on("load", () => {
-            // Add points to map as a GeoJSON source.
-            this.map.addSource("points", {
-                type: "geojson",
-                data: this.config.data
-            });
+        this.map.on("load", this.configureMap.bind(this));
 
-            // Add a layer to the map to render the GeoJSON points.
-            this.map.addLayer({
-                id: "points",
-                type: "circle",
-                source: "points",
-                paint: {
-                    "circle-radius": 5,
-                    "circle-color": "#ff5500",
-                    "circle-stroke-width": 1,
-                    "circle-stroke-color": "#000"
-                }
-            });
+        this.map.on("click", "unclustered-point", this.onSingleMarkerClick.bind(this));
+        this.map.on('click', 'clusters', this.onClusterClick.bind(this));
+
+        this.map.on("mouseenter", "clusters", this.makeMousePointer.bind(this));
+        this.map.on("mouseenter", "unclustered-point", this.makeMousePointer.bind(this));
+        this.map.on("mouseleave", "clusters", this.resetMouse.bind(this));
+        this.map.on("mouseleave", "unclustered-point", this.resetMouse.bind(this));
+    }
+
+    configureMap(){
+        // Add points to map as a GeoJSON source.
+        this.map.addSource(MapView.SOURCE_NAME, {
+            type: "geojson",
+            data: this.config.data,
+            cluster: true,
+            clusterMaxZoom: 14,
+            clusterRadius: 35
         });
 
-        // Show a popup when clicking on a point.
-        this.map.on("click", "points", event => {
-            console.log(event);
-            new mapboxgl.Popup()
-                .setLngLat(event.lngLat)
-                .setHTML(`Clicked on ${event.features.length} feature(s).`)
-                .addTo(this.map);
+        this.map.addLayer({
+            id: "clusters",
+            type: "circle",
+            source: MapView.SOURCE_NAME,
+            filter: ["has", "point_count"],
+            paint: {
+                "circle-color": "#51bbd6",
+                "circle-radius": 15,
+                "circle-stroke-width": 2
+            }
         });
 
-        // Change the cursor to a pointer when the mouse is over the points layer.
-        this.map.on("mouseenter", "points", () => {
-            this.map.getCanvas().style.cursor = "pointer";
+        this.map.addLayer({
+            id: "cluster-count",
+            type: "symbol",
+            source: MapView.SOURCE_NAME,
+            filter: ["has", "point_count"],
+
+            layout: {
+                "text-field": "{point_count_abbreviated}",
+                "text-font": ["Open Sans Bold"],
+                "text-size": 12
+            }
         });
 
-        // Change it back to a pointer when it leaves.
-        this.map.on("mouseleave", "points", () => {
-            this.map.getCanvas().style.cursor = "";
+        this.map.addLayer({
+            id: "unclustered-point",
+            type: "circle",
+            source: MapView.SOURCE_NAME,
+            filter: ["!has", "point_count"],
+            paint: {
+                "circle-color": "#11b4da",
+                "circle-radius": 7,
+                "circle-stroke-width": 1,
+                "circle-stroke-color": "#fff"
+            }
         });
     }
+
+    makeMousePointer() {
+        this.map.getCanvas().style.cursor = "pointer";
+    }
+
+    resetMouse() {
+        this.map.getCanvas().style.cursor = "";
+    }
+
+    onClusterClick(e){
+        const features = this.map.queryRenderedFeatures(e.point, {layers: ['clusters']});
+        const clusterId = features[0].properties.cluster_id;
+        this.map.getSource(MapView.SOURCE_NAME).getClusterExpansionZoom(clusterId, (err, zoom) => {
+            if (err)
+                return;
+
+            this.map.easeTo({
+                center: features[0].geometry.coordinates,
+                zoom: zoom
+            });
+        });
+    }
+
+    onSingleMarkerClick(event){
+        new mapboxgl.Popup()
+            .setLngLat(event.lngLat)
+            .setHTML(`Clicked on a feature with id = ${event.features[0].properties.id}`)
+            .addTo(this.map);
+    }
 }
+
+MapView.SOURCE_NAME = "points";
